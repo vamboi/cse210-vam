aad2de27cacae33f49e56ff76f5a37cb81dc0230
vamboi <elijah.vamboi2000@gmail.com>
2024-09-19 04:35:18 +0000

    Prep 2 Assignment

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,45 @@
 using System;
+using System.Security.Cryptography;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your percentage?");
+        string answer = Console.ReadLine();
+        int percent = int.Parse(answer);
+
+        string letter = "";
+
+        
+
+        if (percent >=90)
+        {
+            letter="A";
+        }
+        else if (percent >=80)
+        {
+            letter="B";
+        }
+        else if (percent >=70)
+        {
+            letter="C";
+        }
+        else if (percent >=60)
+        {
+            letter ="D";
+        }
+        else
+        {
+            letter ="F";
+        }
+        Console.WriteLine($"Your grade is: {letter}");
+        if (percent >=70)
+        {
+            Console.WriteLine("Congratulations!You pass!!");
+        }
+        else
+        Console.Write("You fail! Better luck next time");
     }
+    
 }
