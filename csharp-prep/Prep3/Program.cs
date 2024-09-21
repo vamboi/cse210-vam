24b9c9d94be7bb6f5bd7712a093ad3f7595f7004
vamboi <elijah.vamboi2000@gmail.com>
2024-09-21 15:39:01 +0000

    Prep 3 Assignment

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,37 @@
 using System;
+using System.Security.Cryptography;
 
 class Program
+
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+       
+        {
+            Console.WriteLine("What is your magic number? ");
+            int magicnumber = int.Parse(Console.ReadLine());
+
+            int guess = 0;
+
+            while (magicnumber > guess)
+            {
+                  Console.Write("What is your guess? ");
+             guess = int.Parse(Console.ReadLine());
+            if(guess < magicnumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guess > magicnumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it right!! ");
+            }
+           }
+        }
     }
 }
+
+    
